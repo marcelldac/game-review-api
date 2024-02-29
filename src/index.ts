3b37738487f6f3be4cdf03123e223e50d813b1f4
marcelldac <dacmarcell@protonmail.com>
2024-02-29 10:30:47 -0300

    style: lint

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,12 @@ import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes";
-import { Game } from "./entity/Game";
 
 AppDataSource.initialize()
   .then(async () => {
-    // create express app
     const app = express();
     app.use(bodyParser.json());
 
-    // register express routes from defined application routes
     Routes.forEach((route) => {
       (app as any)[route.method](
         route.route,
@@ -34,24 +31,7 @@ AppDataSource.initialize()
       );
     });
 
-    // setup express app here
-    // ...
-
-    // start express server
     app.listen(3000);
-
-    // insert new users for test
-    // await AppDataSource.manager.save(
-    //   AppDataSource.manager.create(Game, {
-    //     name: "God of War",
-    //   })
-    // );
-
-    // await AppDataSource.manager.save(
-    //   AppDataSource.manager.create(Game, {
-    //     name: "Days Gone",
-    //   })
-    // );
 
     console.log(
       "Express server has started on port 3000. Open http://localhost:3000/games to see results"
